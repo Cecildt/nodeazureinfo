ff20ee5e0aefbedf687310bb94ac89d3ae6ca586
Cecil <cecildt@gmail.com>
2013-08-09 22:41:44 +0200

    Add server and package files.

---
+++ server.js
@@ -0,0 +1,12 @@
+﻿var http = require('http');
+
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    var title = process.title,
+        version = process.version;
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(title + " - " + version);
+}).listen(port);
+console.log("Server listening on port %d", port);
